f024b9cd2c2117572dc5401b9327d597dce6947d
Arthur Halbart <arthur.halbart@gmail.com>
2025-05-26 02:20:37 +0200

    Refont_Design

--- workflow-laravel-mix-6-2/src/scripts/app.js
+++ workflow-laravel-mix-6-2/src/scripts/app.js
@@ -1,1 +1,12 @@
 console.info('Hello world');
+
+  document.addEventListener('mousemove', function(e) {
+    const follower = document.querySelector('.element__graphique__fond');
+    const size = 989; 
+  
+    const x = e.clientX - size / 2;
+    const y = e.clientY - size / 2;
+  
+    follower.style.left = x + 'px';
+    follower.style.top = y + 'px';
+  });
