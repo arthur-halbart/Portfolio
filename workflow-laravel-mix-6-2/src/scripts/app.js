7d5d58bebe1303bcebf1f6d3f23db3b598cdcfc6
Arthur Halbart <arthur.halbart@gmail.com>
2025-05-28 09:25:53 +0200

    Avancement CS suit

--- workflow-laravel-mix-6-2/src/scripts/app.js
+++ workflow-laravel-mix-6-2/src/scripts/app.js
@@ -19,6 +19,28 @@ var elementFond = document.querySelector('.element__graphique__fond')
   });
 
 
+  ///////////////////////////////////////////////////////
+  const canvas = document.getElementById('noiseCanvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  function generateNoise() {
+  const imageData = ctx.createImageData(canvas.width, canvas.height);
+  const buffer = new Uint32Array(imageData.data.buffer);
+  for (let i = 0; i < buffer.length; i++) {
+    buffer[i] = (255 << 24) | // alpha
+                (Math.random() * 255 << 16) | // red
+                (Math.random() * 255 << 8) | // green
+                (Math.random() * 255); // blue
+  }
+  ctx.putImageData(imageData, 0, 0);
+  requestAnimationFrame(generateNoise);
+}
+
+generateNoise();
+
+
 
 
 ////////////////////////////////////////////////////////////////////:
@@ -151,3 +173,7 @@ button4.addEventListener('mouseleave', function() {
         follower.style.top = y + 'px';
       });
 });
+
+
+///////////////////////////////////////////////////////////////////////////////
+
